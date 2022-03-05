753f4e3040fde6bb9af906cde4e7467aec86833a
madhu-minion <madhuminion20@gmail.com>
2022-03-05 10:58:42 +0530

    UI design completed

---
+++ app/src/main/java/com/greymatter/heresthedrink/CategoryAdapter.java
@@ -0,0 +1,12 @@
+package com.greymatter.heresthedrink;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CategoryAdapter  {
+
+}
