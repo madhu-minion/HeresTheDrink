a7e832d7de705bd8a563de72700afb829c588e5d
IyyappanPalanivel <iyyappan.p@technotackle.com>
2022-03-09 22:33:09 +0530

    -bugs fixed

--- app/src/main/java/com/greymatter/heresthedrink/BluePrintActivity.java
+++ app/src/main/java/com/greymatter/heresthedrink/BluePrintActivity.java
@@ -3,6 +3,7 @@ package com.greymatter.heresthedrink;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -19,5 +20,13 @@ public class BluePrintActivity extends AppCompatActivity {
         blueprint = findViewById(R.id.blueprint);
 
         Picasso.get().load(getIntent().getStringExtra("url")).into(blueprint);
+
+        findViewById(R.id.back_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
     }
 }
